73f2867e30e233da17da3f36d7f5ff954e590fad
Nick Florin <nickmflorin@gmail.com>
2024-05-29 19:27:52 -0400

    Add some tests for edge cases

--- src/__tests__/unit/pdf/types.test.ts
+++ src/__tests__/unit/pdf/types.test.ts
@@ -10,6 +10,12 @@ describe("constructPdfUrl properly returns", () => {
   it("properly returns when multiple trailing slashes are present", () => {
     expect(constructPdfUrl("http://example.com///", "test")).toBe("http://example.com/test.pdf");
   });
+  it("properly throws when ID is empty", () => {
+    expect(() => constructPdfUrl("http://example.com///", "")).toThrow();
+  });
+  it("properly throws when domain is not present", () => {
+    expect(() => constructPdfUrl("http://", "")).toThrow();
+  });
   it("properly throws when ID is invalid", () => {
     expect(() => constructPdfUrl("http://example.com///", "test/")).toThrow();
   });
